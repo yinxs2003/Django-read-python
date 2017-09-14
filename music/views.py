38b20cdc6353c8e75715eca61754ba77fb7c687f
yinxuesong <yxs1112003@163.com>
2017-09-14 23:12:41 +0800

    更新index页面

--- music/views.py
+++ music/views.py
@@ -4,12 +4,12 @@ from music.models import Album
 from polls.models import Question
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request, 'music/albumIndex.html', context)
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     return render(request, 'music/albumIndex.html', context)
 
 
 def albumIndex(request):
